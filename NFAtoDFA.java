4b96d581750d64c36d3b135082f2be95f2860739
AlecSKlein <Alec@Alecs-MacBook-Air.local>
2015-04-01 12:45:04 -0500

    Pushing filestripper

---
+++ NFAtoDFA.java
@@ -0,0 +1,140 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class NFAtoDFA
+{
+	public class NFA
+	{
+		//First line
+		public int numStates;
+		//Second Line
+		public ArrayList<Character> alphabet;
+		//Third + numStates lines
+		public ArrayList<ArrayList<Integer> > allStates;
+		//Fourth + numStates line
+		public int initState;
+		//Last line
+		public ArrayList<Integer> finalStates;
+		
+		public NFA()
+		{
+			numStates = 0;
+			alphabet = new ArrayList<Character>();
+			allStates = new ArrayList<ArrayList<Integer> >();
+			initState = 0;
+			finalStates = new ArrayList<Integer>();
+		}
+
+		public NFA(int n, ArrayList<Character> sigma, ArrayList<ArrayList<Integer> > states, int initial, ArrayList<Integer> fStates)
+		{
+			numStates = n;
+			for(char c: sigma)
+				alphabet.add(c);
+			for(ArrayList<Integer> state: states)
+				allStates.add(state);
+			initState = initial;
+			for(Integer fState: fStates)
+				finalStates.add(fState);
+		}
+	}
+
+	public ArrayList<String> readFromFile(String filename)
+	{
+		ArrayList<String> allLines = new ArrayList<String>();
+		File file = new File(filename);
+		try
+		{
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			while((line = br.readLine()) != null)
+			{
+				line = line.replace("\t", "");
+				line = line.replace(" ", "");
+				allLines.add(line);
+			}
+		}
+		catch(IOException e)
+		{
+			//
+		}
+		return allLines;
+	}
+
+	public boolean charIsAlpha(char c)
+	{
+		if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+			return true;
+		return false;
+	}
+
+	public NFA extractNFA(ArrayList<String> lines)
+	{
+		/*
+		
+			Rules for an NFA file
+		1)	First line = 	number of states, n
+		2)	Second line = 	the alphabet, sigma
+		3)	Third - n+2 = 	the individual states
+		4)	N+2th		= 	the initial/starting state
+		5)	N+3th		=	the set of final states
+
+		*/
+
+		NFA nfa = new NFA();
+
+		String lastLine = lines.remove(lines.size()-1);
+
+		int n 		= 	nfa.numStates = Integer.parseInt(lines.remove(0));
+		int init 	= 	nfa.initState = Integer.parseInt(lines.remove(lines.size()-1));
+		String l2 	= 	lines.remove(0);
+
+		char letter = '\0';
+		for(int i = 0; i < l2.length(); i++)
+		{
+			letter = l2.charAt(i);
+			if(charIsAlpha(letter))
+				nfa.alphabet.add(letter);
+		}
+
+		System.out.println("Remaining in list:");
+		for(String s: lines)
+			System.out.println(s);
+
+		extractStates(n, lines);
+
+		return nfa;
+	}
+
+	public void extractStates(int size, ArrayList<String> states)
+	{
+		//This loop modifies the remaining states
+		//{} becomes N for Null
+		//}{ becomes S for Space
+		//Numbers within the states are separated by commas
+		//Ex) {1,2,3}S{2,3}S{N}
+		for(int i = 0; i < states.size(); i++)
+			states.set(i, ((states.get(i).split(":")[1]).replace("{}", "{N}")).replace("}{", "}S{"));
+
+		for(String s: states)
+			System.out.println(s);
+
+		return;
+	}
+
+	public static void main(String[] args)
+	{
+		NFAtoDFA test = new NFAtoDFA();
+		String filename;
+		if(args.length > 0)
+			filename = args[0];
+		else
+			filename = "nfa1.nfa";
+		ArrayList<String> lines = test.readFromFile(filename);
+
+		test.extractNFA(lines);
+	}
+}
