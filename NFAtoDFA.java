832c7addb0609e5f48164933fce93ede01ea77d2
AlecSKlein <Alec@alecs-air.attlocal.net>
2015-04-05 12:36:58 -0500

    Finished state extraction

--- NFAtoDFA.java
+++ NFAtoDFA.java
@@ -13,7 +13,7 @@ public class NFAtoDFA
 		//Second Line
 		public ArrayList<Character> alphabet;
 		//Third + numStates lines
-		public ArrayList<ArrayList<Integer> > allStates;
+		public ArrayList<ArrayList<ArrayList<Integer> > > allStates;
 		//Fourth + numStates line
 		public int initState;
 		//Last line
@@ -23,17 +23,17 @@ public class NFAtoDFA
 		{
 			numStates = 0;
 			alphabet = new ArrayList<Character>();
-			allStates = new ArrayList<ArrayList<Integer> >();
+			allStates = new ArrayList<ArrayList<ArrayList<Integer> > >();
 			initState = 0;
 			finalStates = new ArrayList<Integer>();
 		}
 
-		public NFA(int n, ArrayList<Character> sigma, ArrayList<ArrayList<Integer> > states, int initial, ArrayList<Integer> fStates)
+		public NFA(int n, ArrayList<Character> sigma, ArrayList<ArrayList<ArrayList<Integer> > > states, int initial, ArrayList<Integer> fStates)
 		{
 			numStates = n;
 			for(char c: sigma)
 				alphabet.add(c);
-			for(ArrayList<Integer> state: states)
+			for(ArrayList<ArrayList<Integer> > state: states)
 				allStates.add(state);
 			initState = initial;
 			for(Integer fState: fStates)
@@ -54,7 +54,8 @@ public class NFAtoDFA
 			{
 				line = line.replace("\t", "");
 				line = line.replace(" ", "");
-				allLines.add(line);
+				if(!line.equals(""))
+					allLines.add(line);
 			}
 		}
 		catch(IOException e)
@@ -91,6 +92,7 @@ public class NFAtoDFA
 		int n 		= 	nfa.numStates = Integer.parseInt(lines.remove(0));
 		int init 	= 	nfa.initState = Integer.parseInt(lines.remove(lines.size()-1));
 		String l2 	= 	lines.remove(0);
+		int alphabetSize;
 
 		char letter = '\0';
 		for(int i = 0; i < l2.length(); i++)
@@ -100,16 +102,18 @@ public class NFAtoDFA
 				nfa.alphabet.add(letter);
 		}
 
-		System.out.println("Remaining in list:");
-		for(String s: lines)
-			System.out.println(s);
+		alphabetSize = nfa.alphabet.size();
 
-		extractStates(n, lines);
+		for(int i = 0; i < alphabetSize; i++)
+			System.out.print(nfa.alphabet.get(i) + "\t");
+		System.out.println();
+
+		nfa.allStates = extractStates(n, alphabetSize, lines);
 
 		return nfa;
 	}
 
-	public void extractStates(int size, ArrayList<String> states)
+	public ArrayList<ArrayList<ArrayList<Integer> > > extractStates(int stateSize, int alphSize, ArrayList<String> states)
 	{
 		//This loop modifies the remaining states
 		//{} becomes N for Null
@@ -119,10 +123,71 @@ public class NFAtoDFA
 		for(int i = 0; i < states.size(); i++)
 			states.set(i, ((states.get(i).split(":")[1]).replace("{}", "{N}")).replace("}{", "}S{"));
 
-		for(String s: states)
-			System.out.println(s);
+		//TODO: Split by S, add to list of lists
+		//TODO: Then split by commas
+		//TODO: Then add each resulting number to a list
+		//TODO: When that list is complete, add that list to the final list
 
-		return;
+		ArrayList<ArrayList<String> > tempStates = new ArrayList<ArrayList<String> >();
+
+		//System.out.println();
+
+		for(int i = 0; i < states.size(); i++)
+		{
+			String[] temp = states.get(i).split("S");
+			ArrayList<String> temp2 = new ArrayList<String>();
+			//System.out.println("New temp");
+
+			for(String s: temp)
+			{
+				//System.out.println("Adding " + s + " to this temp");
+				temp2.add(s);
+			}
+			tempStates.add(temp2);
+		}
+
+		//System.out.println();
+
+		ArrayList<ArrayList<ArrayList<Integer> > > all_states = new ArrayList<ArrayList<ArrayList<Integer> > >();
+		for(int i = 0; i < tempStates.size(); i++)
+		{
+			ArrayList<ArrayList<Integer> > full_state = new ArrayList<ArrayList<Integer> >();
+			for(int j = 0; j < tempStates.get(i).size(); j++)
+			{
+				ArrayList<Integer> single_state = new ArrayList<Integer>();
+				String temp = tempStates.get(i).get(j);
+				//System.out.print(temp);
+				//System.out.println("\nStripping");
+				temp = temp.replace("{", "").replace("}", "");
+				//System.out.println(temp + "\nStripped\n");
+				String[] transitionStates = temp.split(",");
+				for(String s: transitionStates)
+				{
+					if(s.equals("N"))
+						single_state.add(-1);
+					else
+						single_state.add(Integer.parseInt(s));
+				}
+				full_state.add(single_state);
+			}
+			all_states.add(full_state);
+		}
+
+		for(int i = 0; i < all_states.size(); i++)
+		{
+			for(int j = 0; j < all_states.get(i).size(); j++)
+			{
+				System.out.print("{");
+				for(int k = 0; k < all_states.get(i).get(j).size(); k++)
+				{
+					System.out.print(all_states.get(i).get(j).get(k) + ",");
+				}
+				System.out.print("}");
+			}
+			System.out.println();
+		}
+
+		return all_states;
 	}
 
 	public static void main(String[] args)
@@ -135,6 +200,6 @@ public class NFAtoDFA
 			filename = "nfa1.nfa";
 		ArrayList<String> lines = test.readFromFile(filename);
 
-		test.extractNFA(lines);
+		NFA demo = test.extractNFA(lines);
 	}
 }
